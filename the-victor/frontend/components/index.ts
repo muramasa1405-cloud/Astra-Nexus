1bc386eb42b9adb54f806cab11d7ded968ef0f4c
muramasa1405-cloud <muramasa1405@gmail.com>
2026-06-13 13:59:11 +0700

    Update index.ts

--- the-victor/frontend/components/index.ts
+++ the-victor/frontend/components/index.ts
@@ -1,1 +1,9 @@
-index.ts
+// Export all components for easy importing
+export { default as ChatInterface } from './ChatInterface';
+export { default as LivePreview } from './LivePreview';
+export { default as VictorControls } from './VictorControls';
+export { default as ProjectManager } from './ProjectManager';
+export { default as History } from './History';
+
+// You can add more components here later
+// export { default as SomeNewComponent } from './SomeNewComponent';
